7672eee7c5af1504986eef843b9ce8468771b8ee
ydic <72564717+ydic@users.noreply.github.com>
2021-09-27 21:46:57 +0900

    Add Edit & Middleware (O- videoSchema.static, X- videoSchema.pre('save',  function(){})
    [Javascript] .join(), .split(), map(), .startsWith() ? TRUE : FALSE /
    [Mongoose] .exec(), <db model>.exists(FILTER), <db model>.findByIdAndUpdate(), videoSchema.static() /
    [CTRL - getEdit] if <handle error> else <handle normal> /
    [PUG] 404

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,5 +1,11 @@
-// 
-import Video from '../models/video';
+/*
+    // [ Mongoose 문법 ] Javascript 문법인 export, import를 활용해 샵(#) 달아주기 코드를 save와 update 컨트롤 함수에서 불러와 사용해도 되지만, 몽구스 내장함수인 videoSchema.static(이름, 함수)를 활용해서 save와 update 작업에서의 샵(#) 달아주기를 처리할 수도 있다
+    import Video, { formatHashtags } from '../models/video';
+*/  
+
+// Video.js에서 export default Video; 한 것을 import 함
+import Video from '../models/video'; 
+
 
 // Controller 모듈 코드 내의 res.render를 통해 views 폴더 이하의 pug파일을 html 코드로 render하여 받아옴
 // express 엔진은 NodeJS를 실행시켜주는 package.json 파일 위치(cwd) 기준으로 views 폴더(src/views)를 바라보기 때문에 별도의 import, export 불필요
@@ -62,30 +68,83 @@ export const watch = async (req, res) => {
   
   // [ Mongoose 문법 ] <db model>.findById 내장함수를 통해 id로 데이터(즉, 영상)을 찾아낼 수 있음
   // [ Javascript 문법 ] async await 문법을 적용하여 .findById 내장함수가 데이터를 찾아주는 시간을 기다리도록 함
-  const video = await Video.findById(idVideo);
-  console.log('watch video---------------',video);
+  // [ Mongoose 문법 ] .exec() 내장함수는 query를 실행시키고 promise를 return 해줌. (async, await 사용중이라면 .exec() 내장함수 표기하지 않아도 .findByID() 단독으로도 query 실행 가능함)
+  
+  const video = await Video.findById(idVideo)
 
-  // [ 질문 ] ??? res 응답할 때 return 붙이고 안붙이고 실행결과 차이? 어떤 것이 옳은 문법? 왜?
+  // 에러 나는 경우를 먼저 if로 처리해주고 else에는 정상적인 케이스에 작동할 코드를 담으면 됨
+  // 사용자가 존재하지 않는 페이지를 검색할 경우도 대비해 return res.render(PUG 파일 (즉,404 관련)) 적절한 응답처리 페이지 만들어놓아야 함
+  if(video === null) {
+        // if(!video) {
+    
+    // ??? [ 주의 ] videoRouter.js의 정규표현식 /:id[0-9a-f]{24} 덕분에 몽고DB? 몽구스? ObjectID 24자리값을 인식하고 다룰 수 있게 됐지만, 글자수 24자이면서 오타(존재하지 않는 ID)는 videoRouter에서 404.pug로까지 데이터 넘어오지만, 글자수 24자 아닌(23이하, 25 이상) 오타값에 대한 페이지 검색시에는 여전히 cannot GET 응답 없는 페이지 나옴. 이 부분도 별도 처리 필요.
+    // [ Javascript 문법 ] function을 끝내야 하므로 if문 안에 return 표기를 반드시 명시해야 함
+    return res.render('404', { pageTitle: 'Video not found.'});  
 
-  return res.render('watch', { pageTitle: video.title, video});  
+  } else {
+    // [ 질문 ] ??? res 응답할 때 return 붙이고 안붙이고 실행결과 차이? 어떤 것이 옳은 문법? 왜?
+
+    // 존재하지 않아 유효하지 않은 id(24바이트 16진수) 값으로 접속 시도하면 영상 검색에 실패했으므로 "비디오는 null이고 null은 title을 갖고 있지 않습니다" --- TypeError: Cannot read property 'title' of null
+    return res.render('watch', { pageTitle: video.title, video});  
+  }
 }
 
 // getEdit 함수는 브라우저에 form을 보여줌
-export const getEdit = (req, res) => {
+export const getEdit = async (req, res) => {
   const idVideo = req.params.id;
   
-  return res.render('edit', { pageTitle: `Editing`, video});
+  // 대문자 Video는 Video.js에서 Schema 정의한 Mongoose Model임
+  // 소문자 video는 db에서 검색한 영상 object임(title, description, hashtags, ...)
+  
+  const video = await Video.findById(idVideo)
+
+  if(!video) {
+    return res.render('404', { pageTitle: 'Video not found.'})
+  } else {
+    return res.render('edit', { pageTitle: `Editing: ${video.title}`, video});
+  }
 };
 
 // postEdit 함수는 변경사항을 저장해줌
-export const postEdit = (req, res) => {
+export const postEdit = async (req, res) => {
   const idVideo = req.params.id;
-  
+    
   // [ Express 문법 ] express 스스로는 form의 body(즉, value)를 처리할 줄 모름. 
   // [ Express 문법 ] route들을 사용하기 전에 middleware를 사용해야 함
   // [ Express 문법 ] server.js에서 express.urlencoded() 내장함수를 middleware로써 기능하도록 app.use(urlencoded( { extended: true } )); 라고 코딩하여 express에게 form을 처리하고 싶다고 알려주면 Javascript 형식으로 변형시켜줘서 우리가 사용할 수 있게 됨
-  const titleVideo = req.body.titleVideoInput;
-  
+  const { title, description, hashtags } = req.body;
+
+  const handlePostEdit = {
+      title,
+      description,
+      
+      // [ Javascript 문법 ] startsWith() 메소드는 어떤 문자열이 특정 문자로 시작하는지 확인하여 결과를 true 혹은 false로 반환합니다.
+      // [ Javascript 문법 ] ??? startsWith() 
+      hashtags: 
+                /*  
+                    
+                          // hashtags.split(',').map((word) => (word.startsWith('#') ? word : `#${word}`)),
+                          // [ javascript 문법 ] 미주알고주알 코드넝쿨을 간결화하기 위해 Video.js 에서 샵(#) 달아주기 기능 함수를 named export 하고 videoController.js에서 import하여 사용함
+                              formatHashtags(hashtags),
+                */
+                // [ Mongoose 문법 ] Javascript 문법인 export, import를 활용해 샵(#) 달아주기 코드를 save와 update 컨트롤 함수에서 불러와 사용해도 되지만, 몽구스 내장함수인 videoSchema.static(이름, 함수)를 활용해서 save와 update 작업에서의 샵(#) 달아주기를 처리할 수도 있다 
+                Video.formatHashtags(hashtags),
+    }
+
+  // [ Mongoose 문법 ] Edit 기능 구현을 위해 video 오브젝트(title, description, hashtags, ...) 전체를 받는 대신에 true/false 여부(즉, 영상 존재 여부)만 받아와도 됨 (즉, <db model>.findById() 대신에 <db model>.exists() 사용이 적절)
+  // [ Mongoose 문법 ] <db model>.exists(아규먼트 자리인 이곳에 id는 받지 않으며 filter(즉, 조건)만 받음)
+  // 오브젝트 id(즉, _id)가 req.params.id와 같은 경우(즉, 조건)를 찾음
+  const videoTrueFalse = Video.exists({ _id : idVideo});
+
+  if(!videoTrueFalse) {
+    return res.render('404', { pageTitle: 'Video not found.'})
+  }
+
+  // [ Mongoose 문법 ] <db model>.findByIdAndUpdate 내장함수를 이용해 _id값에 해당하는 db데이터에 대해 update 처리함
+  // [ Mongoose 문법 ] 주의: .findByIdAndUpdate 기능을 위한 Pre Middleware는 없음 / findByIdAndUpdate는 findOneAndUpdate를 호출함 / findOneAndUpdate를 위한 Middleware는 있음 / findOneAndUpdate는 save hook을 호출하는 기능은 없으며 update 하려는 문서에 접근할 수 없음 / 
+  await Video.findByIdAndUpdate(idVideo, handlePostEdit);
+
+  console.log(req.body);
   return res.redirect(`/videos/${idVideo}`);
 };
 
@@ -121,9 +180,21 @@ export const postUpload = async (req, res) => {
       // [ Mongoose 문법 ] models/Video.js 에서 db 스키마 설정할 때 createdAt: { type: Date, required: true, default: Date.now }와 같이 default로 Date.now 함수 값 부여하도록 이미 설정했으므로 VideoController.js의 Video.create() 내장함수 내에서 createdAt 값 동작 자체를 처리하지 않아도 됨
           // createdAt: Date.now(),
 
-      // ??? 질문- 사용자가 쉼표 다음에 빈 칸 넣어서 입력했다면 어떻게 빈 칸 자동 제거?
-      hashtags: hashtags.split(',').map(word => `#${word}`),
+      // [ Mongoose 문법 ] 미들웨어(Middleware) / pre, post, hook 기능을 통해 object가 db에 저장 및 업데이트 하기 전 또는 후 시점에 사용자의 입력값에 대한 사전처리를 하거나 체크를 해야하는 경우가 있음
 
+
+      // ??? 질문- 사용자가 쉼표 다음에 빈 칸 넣어서 입력했다면 어떻게 빈 칸 자동 제거?
+      // Video.js 파일의 db schema 설정 코드에서 hashtags 속성을 string 타입의 array로 지정했었음 (즉, hashtags: [{ type: String, trim: true }])
+      // [ Javascript 문법 ] .split(), .map() 내장함수 체이닝 하지 않은 hashtags에는 사용자가 쉼표(,)로 구분해 입력한 값들이 ['a,b,c']와 같은 array[0] 값(1 string element)으로만 처리됨
+      hashtags: 
+                /*  
+                    
+                          // hashtags.split(',').map((word) => (word.startsWith('#') ? word : `#${word}`)),
+                          // [ javascript 문법 ] 미주알고주알 코드넝쿨을 간결화하기 위해 Video.js 에서 샵(#) 달아주기 기능 함수를 named export 하고 videoController.js에서 import하여 사용함
+                              formatHashtags(hashtags),
+                */
+                // [ Mongoose 문법 ] Javascript 문법인 export, import를 활용해 샵(#) 달아주기 코드를 save와 update 컨트롤 함수에서 불러와 사용해도 되지만, 몽구스 내장함수인 videoSchema.static(이름, 함수)를 활용해서 save와 update 작업에서의 샵(#) 달아주기를 처리할 수도 있다 
+                Video.formatHashtags(hashtags),
       })
 
       // [ Mongoose 문법 ] .save()는 promise를 return 해줌. (즉, save 되는 순간 db에 기록되고 저장되려면 시간이 걸리므로 작업이 끝날 때까지 기다려줘야 함)
--- src/models/Video.js
+++ src/models/Video.js
@@ -1,12 +1,17 @@
 import mongoose from 'mongoose';
 
+      /*
+          // [ Mongoose 문법 ] Javascript 문법인 export, import를 활용해 샵(#) 달아주기 코드를 save와 update 컨트롤 함수에서 불러와 사용해도 되지만, 몽구스 내장함수인 videoSchema.static(이름, 함수)를 활용해서 save와 update 작업에서의 샵(#) 달아주기를 처리할 수도 있다
+          export const formatHashtags = (hashtags) => hashtags.split(',').map((word) => (word.startsWith('#') ? word : `#${word}`));
+      */
+
 const videoSchema = new mongoose.Schema({
 
   // [ Mongoose 문법 ] db 스키마에서 minlength, maxlength 설정시 사용자와 해당 코드가 제대로 연결되지 않으면 에러 발생할 수 있음. 
-  // [ Mongoose 문법 ] PUG 파일의 HTML form에서도 minlength, maxlength 설정할 수 있는데 굳이 왜 db 스키마에서도 설정해야 할까? 
+  // [ Mongoose 문법 ] PUG 파일(upload.pug)의 HTML form에서도 minlength, maxlength 설정할 수 있는데 굳이 왜 db 스키마에서도 설정해야 할까? 
   // [ Mongoose 문법 ] 정답은 둘 다 해야함 (upload.pug 페이지는 사용자가 개발자 도구로 HTML 재편집해 글자 수 제한이 풀릴 수 있음 / 이런 사용자로부터 사이트를 보호해야 하므로 db 스키마에도 글자 수 제한을 설정해야 함)
   title: {type: String, required: true, trim: true, maxlength: 80},
-  description: { type: String, required: true, trim: true, minlength: 20 },
+  description: { type: String, required: true, trim: true, minlength: 10 },
       
   // [ Mongoose 문법 ] createdAt: Date 라고만 스키마를 만들고 { required: true } 속성을 기재하지 않으면 db 형식 코드(사용자 직접 입력하지 않는 자동 값 부여 내장항목 -예: Date)나 사용자 입력값에 대한 데이터 유효성 검사를 할 수 없음
   // [ Mongoose 문법 ] { required: true } 속성을 기재하면 내부 자동 부여 값(예-Date)이든 사용자 입력값이든 유효성 검사 가능해지고 오류가 있는 submit에 대해서는 db에 저장시키지 않음 ValidationError: Video validation failed: createdAt: Path `createdAt` is required.
@@ -22,6 +27,26 @@ const videoSchema = new mongoose.Schema({
   },
 });
 
+// [ Mongoose 문법 ] 주의: 미들웨어(Middleware) 코드는 model 코드 이전에 작성되어야 함
+// [ Mongoose 문법 ] 미들웨어(Middleware) / pre, post, hook 기능을 통해 object가 db에 저장 및 업데이트 하기 전 또는 후 시점에 사용자의 입력값에 대한 사전처리를 하거나 체크를 해야하는 경우가 있음
+// [ Mongoose 문법 ] 미들웨어(Middleware) / pre, post, hook 코드 내에서 this는 저장하려는 문서를 가리킴
+// 저장(save) 되기 전에(pre) Middleware(videoSchema)로 호출될 async function
+
+      /*  // 일타이피(즉, save도 처리할 수 있고 update도 처리할 수 있는) 불가한 반쪽 짜리 기능 코드이므로 미사용
+          // [ Mongoose 문법 ] Middleware 기능을 하는 videoSchema.pre('save', function(){}) 함수가 가진 기능만으로는 edit(즉, update) 작업에서의 hashtags 샵(#) 달아주기를 해 낼 수 없음
+          videoSchema.pre('save', async function(){
+            // [ Javascript 문법 ] .split(), .map() 내장함수 체이닝 하지 않은 hashtags에는 사용자가 쉼표(,)로 구분해 입력한 값들이 ['a,b,c']와 같은 array[0] 값(1 string element)으로만 처리 됨
+            // 주의 : 본 Middleware 코드(즉, VideoSchema.pre())는 upload 작업에만 유효하므로 edit(즉, update) 작업을 위한 Middleware가 하나 더 필요함
+            // 즉, input에 입력된 값이 하나의 emelment로 array에 입력되기 때문에 그 하나의 element(예- ['a,b,c'])를 .split().map() 내장함수를 체이닝시켜서 여러 element를 가진 array로 변환(예- ['a','b','c'])시켜야 함
+            this.hashtags = this.hashtags[0].split(',').map((word) => word.startsWith('#') ? word : `#${word}` )
+          })
+      */
+
+videoSchema.static('formatHashtags', function(hashtags){
+  return hashtags.split(',').map((word) => (word.startsWith('#') ? word : `#${word}`));
+})
+
+// [ Mongoose 문법 ] 주의: model 코드는 미들웨어(Middleware) 코드 이후에 작성되어야 함
 const Video = mongoose.model('Video', videoSchema);
 
 export default Video;
