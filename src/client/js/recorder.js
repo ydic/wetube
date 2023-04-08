1bf2df7590ff8a6dd669805a977eab9b6a9a10e8
ydic <icgyg102@gmail.com>
2023-04-09 00:43:32 +0900

    #13.4 a.href = videoFile; & a.download = 'ABC.webm';
    [ Javascript ] let videoFile;
    
    [ Web API & Javascript ] const handleDownload = () => {
        const a = document.createElement('a');
    
        a.href = videoFile;
    
        a.download = 'MyRecording.webm';
    
        document.body.appendChild(a);
    
        a.click();
    
        handleReadyToStart();
    }
    
    [ Javascript ] startBtn.addEventListener('click', handleDownload);

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -9,12 +9,53 @@ let stream;
 // [ Javascript 문법 ] MediaRecorder 값을 받는 recorder 변수를 여러 함수에서 용도에 맞게 사용하기 위해 전역 변수인 let recorder; 형태로 코드 최상단에 선언하는 방식으로 개선함
 let recorder;
 
+
+// [ Web API 문법 ] URL.createObjectURL(event.data); 통해 녹화된 영상 파일을 가리키는 URL 생성(즉, 브라우저 메모리상 존재하는 URL 값)
+// [ Javascript 문법 ] 녹화된 영상 파일을 가리키는 URL 값을 여러 함수에서 용도에 맞게 사용하기 위해 전역 변수인 let videoFile; 형태로 코드 최상단에 선언하는 방식으로 개선함
+let videoFile;
+
+// 자체보완함 --- ★★★ --- 녹화된 영상 다운로드 후에 새로운 녹화가 가능한 환경으로 초기화 되도록 자체보완함 (즉, 녹화 버튼 UI 및 비디오 스트림 데이터)
+const handleReadyToStart = () => {
+    startBtn.innerText = 'Start Recording';
+    startBtn.removeEventListener('click', handleDownload);
+    startBtn.addEventListener('click', handleStart);
+        
+    video.src = null;
+    video.srcObject = stream;
+    video.play();
+}
+
+const handleDownload = () => {
+    const a = document.createElement('a');
+
+    // [ Web API 문법 ] URL.createObjectURL(event.data); 통해 녹화된 영상 파일을 가리키는 URL 생성(즉, 브라우저 메모리상 존재하는 URL 값)
+    // [ Javascript 문법 ] a.href = videoFile; 통해, 사용자가 startBtn.innerText = 'Download Recording'; 클릭 시, 녹화된 영상 파일을 가리키는 URL 값을 가짜 a태그의 href 속성에 주입함
+    // [ Web API 문법 ] a태그 href 속성에 담기는 URL 값 예- blob:http://localhost:4000/9904ec82-f144-4622-a96f-a93ca0314fb3
+    a.href = videoFile;
+
+    // [ Javascript 문법 ] a태그에 .download 속성 적용을 통해, a.click(); 통해 a태그 클릭을 가짜로 발생시켜, 녹화된 영상이 다운로드 되도록 함
+    // [ Javascript 문법 ] a.download = 'MyRecording.webm'; 라고 사전 지정했으므로 파일명은 MyRecording / 파일확장자는 .webm 으로 저장됨
+    // [ Javascript 문법 ] 즉, 사용자가 직접 영상 요소 위에서 마우스 우클릭하여 저장하듯이 기능하도록, a.click(); 통해 a태그 클릭을 가짜로 발생시켜 .download 속성을 동작시킴
+    // [ Web API 문법 ] a태그의 href 속성과 download 속성에 담기는 값 예- <a href="blob:http://localhost:4000/9904ec82-f144-4622-a96f-a93ca0314fb3" download="MyRecording.webm"></a>
+    a.download = 'MyRecording.webm';
+    // [ FFmpeg & Web API 문법 ] 브라우저단 비호환으로 폐기된 코드 a.download = 'MyRecording.mp4';
+    
+    // [ Javascript 문법 ] 가짜 클릭 발생시키는 용도의 a태그이므로, 프론트엔드단 전후맥락/부모관계 전혀 따질 필요없이 HTML body 단에 a태그 생성시키기
+    document.body.appendChild(a);
+    
+    // [ Javascript 문법 ] a.click(); 통해 a태그 클릭을 가짜로 발생시켜, 녹화된 영상이 다운로드 되도록 함
+    a.click();
+
+    // 자체보완함 --- ★★★ --- 녹화된 영상 다운로드 후에 새로운 녹화가 가능한 환경으로 재세팅함 (즉, 녹화 버튼 UI 및 비디오 스트림 데이터)
+    handleReadyToStart();
+}
+
 const handleStop = () => {
     // [ Javascript 문법 ] .removeEventListener() 및 .addEventListner() 통해 Toggle 방식으로 동작하도록 UI 와 Event 구성
     // [ Javascript 문법 ] (즉, startBtn 요소에 클릭이벤트 발생시, 실행시킬 함수가 교대로 교체되어 연계되도록 구성)
-    startBtn.innerText = 'Start Recording';
+    startBtn.innerText = 'Download Recording';
     startBtn.removeEventListener('click', handleStop);
-    startBtn.addEventListener('click', handleStart);
+    startBtn.addEventListener('click', handleDownload);
 
     // [ Web API & Javascript 문법 ] handleStop 함수에 recorder.stop(); 코드를 배치하여 녹화 중지 버튼 누르면 녹화 종료되도록 코드 수정함
     // [ Web API & Javascript 문법 ] 즉, 녹화 시작 버튼 누르고 10초 경과 후 setTimeout(() => { recorder.stop(); }, 10000) 코드 통해 녹화 종료시키는 방식은 폐기함
@@ -36,6 +77,7 @@ const handleStart = () => {
     // [ Web API 문법 ] MediaRecorder 객체 내의 state: "inactive" 상태이지만, stream 은 받아 오고 있는 상태(즉, 녹화할 준비가 되어 있으나 비활성 상태)
     // [ Javascript 문법 ] MediaRecorder 값을 받는 recorder 변수를 여러 함수에서 용도에 맞게 사용하기 위해 전역 변수인 let recorder; 형태로 코드 최상단에 선언하는 방식으로 개선함
     recorder = new MediaRecorder(stream);
+    // [ FFmpeg & Web API 문법 ] 브라우저단 비호환으로 폐기된 코드 recorder = new MediaRecorder(stream, { mimeType: "video/mp4"});
     
     // [ Web API 문버 ] recorder.stop(); 통해 dataavailable 이벤트(즉, MediaRecorder 전용 이벤트 중 하나) 가 Web API 구조적으로 자동 실행됨
     // [ Web API 문법 ] dataavailable 이벤트 내에는 녹화된 최종 비디오 데이터 값이 Web API 구조적으로 포함되어 있음
@@ -50,7 +92,7 @@ const handleStart = () => {
         // [ Web API 문법 ] URL.createObjectURL(event.data); 통해 녹화된 영상 파일을 가리키는 URL 생성
         // [ Web API 문법 ] URL.createObjectURL(event.data); 통해 생성된 URL 은 브라우저 메모리 영역에서만 이용 가능 (즉, 웹사이트 상에 존재하는 URL 속성이 아님)
         // [ Web API 문법 ] 즉, 실제 호스팅 URL 만든 것이 아니라 브라우저 메모리 상에 녹화된 영상 파일을 저장해 두고, 브라우저가 그 파일에 접글할 수 있는 URL(즉, 주소값) 을 발행해 주었음
-        const videoFile = URL.createObjectURL(event.data);
+        videoFile = URL.createObjectURL(event.data);
         console.log('URL.createObjectURL(event.data); --- ', videoFile);
 
         // [ Web API 문법 ] video.src = videoFile; 통해서 video 태그(즉, HTML) 에 대해 녹화된 영상 파일을 주입하고, video.srcObject = null; 통해서 기존에 연결돼 있던 실시간 카메라 스트림 데이터는 차단함
