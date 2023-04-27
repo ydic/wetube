36f78204e022d4cf469f47f40a53588cb028f737
ydic <icgyg102@gmail.com>
2023-04-27 15:48:01 +0900

    #16.7 if(status === 201){ addComment(text); }
    [ Web API ] const response = await fetch();
    const status = response.status;
    if(status === 201){
    addComment(text);
    }
    
    [ Javascript ] .querySelector(".video__comments ul");
    .createElement("li");
    .className = "fas fa-comment";
    .appendChild(spanX);
    .prepend();

--- src/client/js/commentSection.js
+++ src/client/js/commentSection.js
@@ -7,7 +7,51 @@ const form = document.querySelector('#commentForm');
 
 const videoContainer = document.querySelector('#videoContainer');
 
-const handleSubmit = (event) => {
+// [ Javascript 문법 ] 프론트엔드(watch.pug)단에 페이크 댓글 만드는 함수
+const addComment = (text) => {
+		// const divUlElement = document.querySelector('.video__comments ul');
+		// console.log('divUlElement ★ ---', divUlElement);
+		const videoComments = document.querySelector(".video__comments ul");
+		
+		// const li = document.createElement('li');
+		const newComment = document.createElement("li");
+
+		// li.classList.add('video__comment');
+		newComment.className = "video__comment";
+
+		// const spanX = document.createElement('span');
+		const span2 = document.createElement("span");
+  
+		// spanX.innerText = '❌';
+		span2.innerText = "❌";
+
+		// li.appendChild(spanX);
+		newComment.appendChild(span2);
+
+		// const iconFa = document.createElement('i');
+		const icon = document.createElement("i");
+
+		// iconFa.classList.add('fas');
+		// iconFa.classList.add('fa-comment');
+		icon.className = "fas fa-comment";
+
+		// li.appendChild(iconFa);
+		newComment.appendChild(icon);
+		
+		// const spanText = document.createElement('span');
+		const span = document.createElement("span");
+
+		// spanText.innerText = `${text}`;
+		span.innerText = ` ${text}`;
+
+		// li.appendChild(spanText);
+		newComment.appendChild(span);
+
+		// divUlElement.prepend(li);
+		videoComments.prepend(newComment);
+}
+
+const handleSubmit = async (event) => {
 	// [ Javascript 문법 ] event.preventDefault(); 통해 브라우저의 default behavior 기본동작(예- 새로고침 / a태그 href(즉, 하이퍼링크))을 중단시킴
 	event.preventDefault();
 
@@ -37,7 +81,8 @@ const handleSubmit = (event) => {
 	// [ Web API 문법 ] handleSubmit() 함수에서는 form 사용하지 않고 fetch() 기법 통해서 Javascript 단에서 POST Request 생성시킴
 	// [ Web API & Express 문법 ] form 기반은 app.use(express.urlencoded({ extended: true })); 미들웨어 코드와 연계
 	// [ Web API & Express 문법 ] fetch() 기반은 한 개의 내용물이라면 app.use(express.text()); / 복수 개의 내용물이라면 app.use(express.json()); 미들웨어 코드와 연계
-	fetch(`/api/videos/${videoId}/comment`, {
+	// [ Javascript 문법 ] async / await 문법 적용하여 const response = await fetch() 형태로 코드 구성한 이유 ---> commentSection.js 의 fetch() 통한 POST 통해 videoController.js 의 createComment 함수 내의 일련의 쿼리 코드들 await Comment.create({}) / video.comments.push(postedComment._id); / video.save(); 통해 댓글 POST 완료 후 return res.sendStatus(201); 코드로부터 상태코드를 리턴 받기까지 시간이 걸리기 때문
+	const response = await fetch(`/api/videos/${videoId}/comment`, {
 		method: 'POST',
 
 		// [ Express 문법 ] headers: { 'Content-Type': 'application/json', } 명시하여 fetch() 통한 POST 요청 데이터의 자료형이 JSON 형식이라는 것을 server.js 의 app.use(express.json()); 미들웨어 코드에게 알려줘야만 req.body 에서 사용자 입력값이 조회 가능함
@@ -56,8 +101,32 @@ const handleSubmit = (event) => {
 		)
 	});
 
+	// [ Javascript 문법 ] commmentSection.js 는 watch.pug 코드 기반의 자바스크립트 코드이므로 const response = await fetch() 실행 끝난 후에 확인하는 console.log(response); 내용은 브라우저단 콘솔에서 조회 가능함
+	console.log('commentSection.js --- response ---', response);
+
+	// [ Javascript 문법 ] console.log(response); 내용 일부분 예시
+	// /* status: 201
+	// 		statusText: "Created"
+	//		type: "basic"
+	// 		url: "http://localhost:4000/api/videos/643f57eea2eb1e0f5e81e3b9/comment" */
+	const status = response.status;
+
+	// [ Javascript 문법 ] 로그인 사용자가 작성한 댓글에 대해 const response = await fetch() 코드 실행 성공한 경우라면 return res.sendStatus(201); 코드 반환 받게 되므로 if(status === 201) 조건 충족시 실질 DB 조회 없는 페이크 댓글 만들어서 브라우저단 사용자에게 보여줌 (즉, 저수준 UX 경험 제공으로 인해 폐기했던 새로고침 코드 window.location.reload() 를 대체하는 기법)
+	// [ Javascript 문법 ] 즉, videoController.js 의 createComment 함수 내의 일련의 쿼리 코드들 await Comment.create({}) / video.comments.push(postedComment._id); / video.save(); 통해 댓글 POST 완료 후 return res.sendStatus(201); 코드 반환
+	// [ Javascript 문법 ] 로그인 사용자가 작성한 댓글에 대해 const response = await fetch() 코드 실행 끝난 후 (X: 성공한 후 / videoController.js 의 createComment 함수 내에서 댓글 달기 시도한 영상이 그 사이에 영상게시자에 의해 삭제되어 await Video.findById(id); 결과가 false 가 되어 return res.sendStatus(404); 상태코드로 반환될 수도 있기 때문) 
+	if(status === 201){
+		// console.log('create fake comment');
+
+		// [ Javascript 문법 ] 프론트엔드(watch.pug)단에 페이크 댓글 만드는 함수
+		addComment(text);
+	}
+
 	// [ Javascript 문법 ] fetch() 통한 POST 요청 후, textarea 내의 사용자 입력값을 비우기
 	textarea.value = '';
+
+				// [ Javascript 문법 ] 로그인 사용자가 작성한 댓글에 대해 const response = await fetch() 코드 실행 끝난 후 window.location.reload() 통해 브라우저 새로고침 하여 방금 POST 한 댓글이 브라우저 상에 실시간으로 보여지도록 만듦
+				// [ Javascript 문법 ] 로그인 사용자가 작성한 댓글에 대해 const response = await fetch() 코드 실행 끝난 후 (X: 성공한 후 / videoController.js 의 createComment 함수 내에서 댓글 달기 시도한 영상이 그 사이에 영상게시자에 의해 삭제되어 await Video.findById(id); 결과가 false 가 되어 return res.sendStatus(404); 상태코드로 반환될 수도 있기 때문) 
+				// window.location.reload() // 새로고침으로 인해 브라우저 깜빡이므로 저수준 UX 경험이어서 코드 폐기
 
 }
 
